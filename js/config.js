5d993e420cbe466e25eacb9fffc69588c7786894
hpham474 <HienPham@my.unt.edu>
2021-11-21 21:28:50 -0600

    Updated config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_3f7Hvf5Jg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4o923f8g8el8aomqg6410en6m4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
